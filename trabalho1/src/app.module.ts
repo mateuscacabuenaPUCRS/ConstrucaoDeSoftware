54be73afd18e9d75af83759c06bdb02a20cd7830
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-10-18 16:03:22 -0300

    Prisma Tutorial and Mock Data
    https://docs.nestjs.com/recipes/prisma

--- trabalho1/src/app.module.ts
+++ trabalho1/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { ApiModule } from './api/api.module';
+import configuration from 'config/configuration';
 import { AppController } from './example/controller/app.controller';
 import { AppService } from './example/service/app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
+    ApiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
