f8602c78793c8381b0cfd1fbc6f80259cef40710
Felipe Freitas <eu@felipefs.dev>
2024-11-21 19:13:52 -0300

    T3 - Infra Folder & Updated Credentials

--- infra/main.tf
+++ infra/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = [".aws/credentials"]
+  shared_credentials_files = ["../.aws/credentials"]
 }
 
 resource "tls_private_key" "my_key" {
