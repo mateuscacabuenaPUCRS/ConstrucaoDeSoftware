e497fbc6f5667d42d86251100a2669119efd7407
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-11-22 15:51:31 -0300

    T3 - Revert Express Import Try-Surrounded

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { writeFileSync } from 'fs';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import express from 'express';
 import { NestFactory } from "@nestjs/core";
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
@@ -36,14 +37,6 @@ async function bootstrap() {
 bootstrap();
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent, context: unknown = {}): Promise<APIGatewayProxyResult> => {
-  let express: any;
-  try {
-    express = require('express');  
-  } catch (e) {
-    console.error('Please run `npm install express`');
-    throw e;
-  }
-
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.init();
